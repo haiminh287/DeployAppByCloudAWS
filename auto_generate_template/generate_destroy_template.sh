bb75f03a32ce99436b9bb92d5b28dba4225383a0
TNieAccStudy <tniestudyacc176@gmail.com>
2025-05-06 12:02:47 +0700

    fix: fix destroy with output for service

--- auto_generate_template/generate_destroy_template.sh
+++ auto_generate_template/generate_destroy_template.sh
@@ -6,39 +6,48 @@ OUTPUT_FILE=""
 USER_ID=""
 BLOCK_ID=""
 SERVICE_ID=""
-SERVICE_TYPE=""
 TFVARS_FILE=""
 
-while getopts "f:o:u:b:s:t:v:" opt; do
+while getopts "f:o:u:b:s:v:" opt; do
   case $opt in
     f) INPUT_FILE="$OPTARG" ;;
     o) OUTPUT_FILE="$OPTARG" ;;
     u) USER_ID="$OPTARG" ;;
     b) BLOCK_ID="$OPTARG" ;;
     s) SERVICE_ID="$OPTARG" ;;
-    t) SERVICE_TYPE="$OPTARG" ;;
     v) TFVARS_FILE="$OPTARG" ;;
     \?)
-      echo "Usage: $0 -f <input_file> -o <output_file> -u <user_id> -b <block_id> -s <service_id> -t <service_type> -v <tfvars_file>"
+      echo "Usage: $0 -f <input_file> -o <output_file> -u <user_id> -b <block_id> -s <service_id>"
       exit 1 ;;
   esac
 done
 
-if [[ -z "$INPUT_FILE" || -z "$OUTPUT_FILE" || -z "$USER_ID" || -z "$BLOCK_ID" || -z "$SERVICE_ID" || -z "$SERVICE_TYPE" || -z "$TFVARS_FILE" ]]; then
+if [[ -z "$INPUT_FILE" || -z "$OUTPUT_FILE" || -z "$USER_ID" || -z "$BLOCK_ID" || -z "$SERVICE_ID" || -z "$TFVARS_FILE" ]]; then
   echo "Missing required arguments."
-  echo "Usage: $0 -f <input_file> -o <output_file> -u <user_id> -b <block_id> -s <service_id> -t <service_type> -v <tfvars_file>"
+  echo "Usage: $0 -f <input_file> -o <output_file> -u <user_id> -b <block_id> -s <service_id> -v <tfvars_file>"
   exit 1
 fi
 
 sed -e "s/_2_user_id_2_/${USER_ID}/g" \
     -e "s/_2_block_id_2_/${BLOCK_ID}/g" \
     -e "s/_2_service_id_2_/${SERVICE_ID}/g" \
-    -e "s/_2_service_type_2_/${SERVICE_TYPE}/g" \
     "$INPUT_FILE" > "$OUTPUT_FILE"
 
 echo "Generated: $OUTPUT_FILE"
 
 # code run final
+
+MODULE_NAME=$(grep -oP 'module "\K[^"]+' "$OUTPUT_FILE")
+echo "MODULE_NAME: $MODULE_NAME"
+
+TARGET="module.$MODULE_NAME"
+tf destroy -target=${TARGET} -var-file=${TFVARS_FILE} --auto-approve
+
+rm "$OUTPUT_FILE"
+
+
+SERVICE_TYPE=$(echo "$MODULE_NAME" | awk -F'-' '{print $3}')
+echo "SERVICE_TYPE: $SERVICE_TYPE"
 
 OUTPUT_TEMPLATE_PATH="./output_template/output_template.tf"
 OUTPUT_TEMPLATE_FILE="./output_file.tf"
@@ -50,19 +59,18 @@ sed -e "s/_2_user_id_2_/${USER_ID}/g" \
     "$OUTPUT_TEMPLATE_PATH" > "$OUTPUT_TEMPLATE_FILE"
 
 OUTPUT_NAME=$(grep -oP 'output "\K[^"]+' "$OUTPUT_TEMPLATE_FILE")
+echo "OUTPUT_NAME: $OUTPUT_NAME"
 
-
-MODULE_NAME=$(grep -oP 'module "\K[^"]+' "$OUTPUT_FILE")
-echo "$MODULE_NAME"
-
-TARGET="module.$MODULE_NAME"
-tf destroy -target=${TARGET} -var-file=${TFVARS_FILE} --auto-approve
-
-echo "$OUTPUT_NAME"
-
+# 1. Pull file tfstate hiện tại
+tf state pull > tmp.tfstate.json
+# 2. Xóa key output mong muốn (output-alice-block_01-host-service_01)
+jq '.serial += 1 | del(.outputs["output-alice-block_01-host-service_01"])' tmp.tfstate.json > new.tfstate.json
+# 3. Push lại tfstate đã chỉnh sửa
+tf state push new.tfstate.json
+# 4. Xoá file tạm
+rm tmp.tfstate.json new.tfstate.json
 
 rm "$OUTPUT_TEMPLATE_FILE"
-rm "$OUTPUT_FILE"
 
 tf init
 
